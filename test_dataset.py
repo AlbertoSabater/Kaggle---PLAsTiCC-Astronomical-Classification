627431b144d2000b2048bdb9cce2edd3da1dd309
Alberto <asabater94@gmail.com>
2018-10-22 15:53:28 +0200

    test_dataset.py updated. n_obs added

--- test_dataset.py
+++ test_dataset.py
@@ -9,7 +9,6 @@ import numpy as np
 
 
 DATA_DIR = './data/'
-DATA_DIR = ''
 
 
 # %%
@@ -28,7 +27,7 @@ plt.hist([ len(g) for i,g in groups_obj ], bins='auto');
 
 dataset['mjd_short'] = dataset['mjd'].apply(lambda x: round(x*10000//10000))
 print('mjd_short calculated')
-group_obs = dataset[:5000].groupby(['object_id', 'mjd_short'])
+group_obs = dataset.groupby(['object_id', 'mjd_short'])
 print('dataset grouped')
 
 def group_process(name, obs):
@@ -54,55 +53,106 @@ def group_process(name, obs):
 	return g_proc
 	
 observations = Parallel(n_jobs=8)(delayed(group_process)(name, obs) for name, obs in tqdm(group_obs, total=len(group_obs)))
+dataset_proc = pd.DataFrame(observations)
+dataset_proc.to_pickle(DATA_DIR + 'processed/observations_1.pckl')
 
 
 # %%
 	
-dataset_proc = pd.DataFrame(observations)
+dataset_proc = pd.read_pickle(DATA_DIR + 'processed/observations_1.pckl')
 dataset_proc['passband_diff'] = 0
 
 
-# %%
-
-obj_ids = dataset_proc.object_id.drop_duplicates()
-for obj_id in tqdm(obj_ids, total=len(obj_ids)):
-	a = dataset_proc[dataset_proc.object_id == obj_id]['mjd_short'].values
+def process_time_columns(dataset, obj_id):
+	# Add time difference between two single observations
+	a = dataset[dataset.object_id == obj_id]['mjd_short'].values
 	diff = [0] + (a[1:] - a[:-1]).tolist()
-	dataset_proc.loc[dataset_proc.object_id == obj_id, 'passband_diff'] = diff
+	dataset.loc[dataset.object_id == obj_id, 'passband_diff'] = diff
 	
 	n_obs = 0
-	for i,r in dataset_proc.loc[dataset_proc.object_id == obj_id].iterrows():
-		dataset_proc.loc[i-1, 'n_obs'] = n_obs
+	# Add number of observation
+	for i,r in dataset.loc[dataset_proc.object_id == obj_id].iterrows():
 		if r.passband_diff > 100: n_obs += 1
-	
+		dataset.loc[i, 'n_obs'] = n_obs
+		
+	return dataset
+
+obj_ids = dataset_proc.object_id.drop_duplicates()
+dataset_proc = Parallel(n_jobs=8)(delayed(process_time_columns)(dataset_proc, obj_id) 
+							for obj_id in tqdm(obj_ids, total=len(obj_ids)))
+
 
 # %%
-	
-# TODO: Añadir número de observación partiendo las obsrevaciones por diff>~100
+
+dataset_proc = pd.read_pickle(DATA_DIR + 'processed/observations_1.pckl')
+dataset_proc['passband_diff'] = 0
+
+# Add time difference between complete observations
+a = dataset_proc['mjd_short'].values
+diff = [0] + (a[1:] - a[:-1]).tolist()
+dataset_proc['passband_diff'] = diff
+dataset_proc.loc[dataset_proc.passband_diff < 0, 'passband_diff'] = 0
+
+
+n_obs = 0
+n_obs_array = []
+for pbd in dataset_proc['passband_diff']:
+	if pbd > 100: n_obs += 1
+	elif pbd == 0: n_obs = 0
+	n_obs_array.append(n_obs)
+
+dataset_proc['n_obs'] = n_obs_array
+
+dataset_proc.to_pickle(DATA_DIR + 'processed/observations_2.pckl')
 
 
 # %%
 
 obj_id = np.random.choice(dataset_proc.object_id.drop_duplicates())
-print(obj_id)
 g = dataset_proc[dataset_proc.object_id==obj_id]
 
-plt.figure(figsize=(20,5));
-plt.plot();
+fig = plt.figure(figsize=(11,6));
+#plt.plot();
 
-for i in g.n_obs.drop_duplicates():
+for s_ind, i in enumerate(g.n_obs.drop_duplicates()):
+	
+	print()
 	
 	data = g[g.n_obs==i]
 	
+	ax = plt.subplot('22{}'.format(s_ind+1));
 	plt.plot(data.mjd_short, data.flux_1);
 	plt.plot(data.mjd_short, data.flux_2);
 	plt.plot(data.mjd_short, data.flux_3);
 	plt.plot(data.mjd_short, data.flux_4);
 	plt.plot(data.mjd_short, data.flux_5);
+	ax.set_title('{} - {}'.format(s_ind, len(data)))
+	
+fig.suptitle(obj_id);
+fig.tight_layout()
+
+# %%
+
+# passband_diff histogram
+plt.hist([dataset_proc.passband_diff], bins='auto');
 
 
 # %%
 
-plt.hist([dataset_proc.passband_diff], bins='auto')
+# Number of observations histogram
+lens = [ max(g.n_obs)+1 for name,g in dataset_proc.groupby('object_id')]
+plt.hist(lens, bins='auto');
+
+for l in set(lens):
+	l_lens = len([ li for li in lens if li==l])
+	print('{}: {:.2f}% - {}'.format(l, l_lens/len(lens), l_lens))
+	
+	
+# %%
+	
+# Length of each observation
+lens = [ len(g) for name,g in dataset_proc.groupby(['object_id', 'n_obs'])]
+plt.hist(lens, bins='auto');
+
 
 
